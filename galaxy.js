5d99f4988f58eed4e00a97277ef7a1ba4f94c65e
Andy Fang <andy@fang.ws>
2014-12-10 17:17:22 -0500

    Same features as previous commit with minor bug fixes.

--- galaxy.js
+++ galaxy.js
@@ -1,52 +1,58 @@
 $('[st-role="trigger"]').on('click',function(event){
-	event.stopPropagation();
-	var $this = $(this),
-		whichGroup = $this.attr('st-group'),
-		$whichTriggers = $('[st-role="trigger"][st-group="' + whichGroup + '"]'),
-		whichState = $this.attr('st-state'),
-		stateRemove = $this.attr('st-remove'),
-		isReady = $this.is('[st-ready]'),
-		isRemove = $this.is('[st-remove]'),
-		isRestricted = $this.is('[st-restrict]'),
-		$toggleTarget = $('[st-role="target"][st-group="' + whichGroup + '"]'),
-		$removeTarget = $('.' + stateRemove + '[st-role="target"]'),
-		$inactiveTriggers = $('[st-remove="' + stateRemove + '"][st-role="trigger"]');
-	if((isReady && ($this.attr('st-ready') === 'true')) || !isReady ){
-		if(isRestricted){
-			if($this.attr('st-restrict') === 'off'){
-				$toggleTarget.removeClass(whichState);
-				$whichTriggers.removeClass('st-activated');
-			}
-			else if($this.attr('st-restrict') === 'on'){
-				$toggleTarget.addClass(whichState);
-				$whichTriggers.addClass('st-activated');
-			}
-		}
-		else {
-			if(!$this.hasClass('st-activated')){
-				$inactiveTriggers.removeClass('st-activated');
-				$whichTriggers.addClass('st-activated');
-			}
-			else{
-				$whichTriggers.removeClass('st-activated');
-				if(isRemove){
-					$whichTriggers.addClass('st-activated');
-				}
-			}
-			$removeTarget.removeClass(stateRemove);
-			$toggleTarget.toggleClass(whichState);
-		}
-	}
+  event.stopPropagation();
+  var $this = $(this),
+      whichGroup = $this.attr('st-group'),
+      $whichTriggers = $('[st-role="trigger"][st-group="' + whichGroup + '"]'),
+      whichState = $this.attr('st-state'),
+      stateRemove = $this.attr('st-remove'),
+      isReady = $this.is('[st-ready]'),
+      isRemove = $this.is('[st-remove]'),
+      isRestricted = $this.is('[st-restrict]'),
+      $toggleTarget = $('[st-role="target"][st-group="' + whichGroup + '"]'),      
+      $removeTarget = $('.' + stateRemove + '[st-role="target"]'),
+      $inactiveTriggers = $('[st-remove="' + stateRemove + '"][st-role="trigger"]');
+  if((isReady && ($this.attr('st-ready') === 'true')) || !isReady ){
+      if(isRestricted){
+        if($this.attr('st-restrict') === 'off'){
+          $toggleTarget.removeClass(whichState);
+          $whichTriggers.removeClass('st-activated');
+        }
+        else if($this.attr('st-restrict') === 'on'){
+          $toggleTarget.addClass(whichState);
+          $whichTriggers.addClass('st-activated');
+        }
+      }
+      else {
+        if(!$this.hasClass('st-activated')){
+          $inactiveTriggers.removeClass('st-activated');
+          $whichTriggers.addClass('st-activated');
+        }
+        else{
+          $whichTriggers.removeClass('st-activated');
+          if(isRemove){
+            $whichTriggers.addClass('st-activated');
+          }
+        }
+        $removeTarget.removeClass(stateRemove);
+        $toggleTarget.toggleClass(whichState);  
+      }
+    
+  }
 });
 
 $('[st-role="activate"]').on('click',function(){
-	var whichToggle = $(this).attr('st-group');
-	$('[st-role="trigger"][st-group="' + whichToggle + '"]').attr('st-ready','true');
+  var whichToggle = $(this).attr('st-group');
+  $('[st-role="trigger"][st-group="' + whichToggle + '"]').attr('st-ready','true');
 });
 
 $('[st-role="target"]').on('click',function(event){
-	event.stopPropagation();
+  event.stopPropagation();
 });
+
+// create anonymous function
+// verify performance concerns - what event handlers are active at any given time - removing and adding bindings
+// declare variables only if they are relevant to the the current situation (reducing lookup)
+// is it ok that this is in the global name space?
 
 
 // Fixed header
